787212b78c8a49cbb446bcd9bde08a7e4caec18a
Alex Moon <alexmoon929@gmail.com>
2017-10-14 19:12:16 -0400

    day 13

---
+++ merge-two-sorted-lists.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/merge-two-sorted-lists/description/
+
+// Merge two sorted linked lists and return it as a new list. 
+// The new list should be made by splicing together the nodes of the first two lists.
+
+/**
+ * Definition for singly-linked list.
+ */
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    if(!l1) return l2;
+    if(!l2) return l1;
+
+    let answer;
+
+    if(l1.val < l2.val) {
+        answer = l1;
+        answer.next = mergeTwoLists(l1.next, l2);
+    }
+    else {
+        answer = l2;
+        answer.next = mergeTwoLists(l1, l2.next);
+    }
+    return answer;
+};
